e04c82307829104a4cce15ef4c6dfeeaff3b810b
Jay North <jaynorthinfo@gmail.com>
2026-01-30 11:53:56 +0000

    when this section appear, use NavRail boarder (_for element <SettingsVie

--- src/components/views/settings-view.tsx
+++ src/components/views/settings-view.tsx
@@ -36,7 +36,7 @@ export function SettingsView({ isOpen, onClose }: SettingsViewProps) {
   return (
     <div
       className={cn(
-        "fixed inset-y-0 right-0 left-0 md:left-20 z-50 transform-gpu transition-transform duration-500 ease-in-out",
+        "fixed inset-y-0 right-0 left-0 md:left-24 z-50 transform-gpu transition-transform duration-500 ease-in-out md:border-l md:border-white/5",
         isOpen ? "translate-y-0" : "translate-y-full"
       )}
       aria-hidden={!isOpen}
